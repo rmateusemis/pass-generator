de78e56c62dc1fe107e1b114a9df0cb61f30219d
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-10-25 13:06:51 +0100

    values tracked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
   password = '';
 
   onButtonClick(): void {
     this.password = 'My password';
+  }
+
+  onChangeUseLetters(): void {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(): void {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(): void {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string): void {
+    console.log(value);
   }
 }
